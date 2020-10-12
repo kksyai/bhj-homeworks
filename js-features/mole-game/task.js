b766d14150e04d4c03929748b6f2a4eb89885997
YanaRu <kksyai@gmail.com>
2020-10-12 22:08:51 +0300

    repair mole

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,28 @@
 let holeNumber = document.querySelectorAll(".hole");
-
 let dead = Number(document.getElementById('dead').innerHTML);
 let lost = Number(document.getElementById('lost').innerHTML);
-console.log("lost", lost);
+
+function reset(){
+    dead = 0;
+    document.getElementById('dead').innerHTML = dead;
+    lost = 0;
+    document.getElementById('lost').innerHTML = lost;
+}
 
 holeNumber.forEach((element)=>{
 element.addEventListener('click', ()=>{
-    document.getElementById('lost').innerHTML =
     element.classList.contains('hole_has-mole') ? 
     document.getElementById('dead').innerHTML = ++dead : 
     document.getElementById('lost').innerHTML = ++lost;
     if(dead == 10) {
+        reset();
         alert('win');
-    }if(lost == 5) {
+
+    }if(lost > 4) {
+        reset();
         alert('loose');
     }
+
 })
 })
 
