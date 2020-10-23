5d9415a67a8da991e02d864630af257a7a620ec5
YanaRu <kksyai@gmail.com>
2020-10-23 20:30:33 +0300

    refactoring element search

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,48 @@
+
 const left = document.querySelector('.slider__arrow_prev');
 const right = document.querySelector('.slider__arrow_next');
 
 const sliders = Array.from(document.querySelectorAll('.slider__item'));
 
-//console.log(sliders.length);
-// let x = 0
+let index = sliders.findIndex(element => {
+    console.log(element);
+    return element.classList.contains('slider__item_active');
+});
 
-let findIndex = function(){
-    let x = 0;
-    sliders.findIndex((element, index) => {
-        if (element.classList.contains('slider__item_active')) {
-            x = index;
-            return x; 
-        }
-    })
-}
-
-right.addEventListener('click', (event)=> {
-    console.log(findIndex(event));
+right.addEventListener('click', ()=>{
+    sliders[index].classList.remove('slider__item_active');
+    (index === sliders.length - 1)?
+        (index = 0,
+        sliders[index].classList.add('slider__item_active')):
+        (sliders[index + 1].classList.add('slider__item_active'));
+    index++;
 })
+
+left.addEventListener('click', ()=>{
+    sliders[index].classList.remove('slider__item_active');
+    index--;
+    (index === 0)?
+        (index = sliders.length - 1,
+        sliders[index].classList.add('slider__item_active')):
+        (sliders[index].classList.add('slider__item_active'));
+})
+
+
+// // let x = 0
+
+// let findIndex = function(){
+//     let x = 0;
+//     sliders.findIndex((element, index) => {
+//         if (element.classList.contains('slider__item_active')) {
+//             x = index;
+//             return x; 
+//         }
+//     })
+// }
+
+// right.addEventListener('click', (event)=> {
+//     console.log(findIndex(event));
+// })
     // console.log(x)
 
 
