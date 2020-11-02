35ba528acee3e08f20afeb77e31f094a78e7461e
YanaRu <kksyai@gmail.com>
2020-11-02 19:32:44 +0300

    document strucrure refactoring

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,4 +1,6 @@
 const body = document.querySelector('body');
+const toolTip = document.querySelector('.tooltip');
+const hasToolTip = Array.from(document.querySelectorAll('.has-tooltip'))
 
 function getCoords(elem) {
     let box = elem.getBoundingClientRect();
@@ -8,28 +10,46 @@ function getCoords(elem) {
     };
   }
 
-function showTooltip(ev){
-    let toolTip = document.createElement('div');
-    toolTip.classList.add('tooltip');
-    toolTip.innerText = ev.target.title;
+// function showTooltip(ev){
+//     let toolTip = document.createElement('div');
+//     toolTip.classList.add('tooltip');
+//     toolTip.innerText = ev.target.title;
+//     toolTip.style.display = "block";  
+
+//     let coords = getCoords(ev.target);
+//     toolTip.style.left = coords.left + "px";
+//     toolTip.style.top = coords.bottom + "px";
+    
+//     ev.target.after(toolTip);
+// }
+
+// body.addEventListener('click', (event)=>{
+//     event.preventDefault();
+//     if(event.target.nextElementSibling == null){
+//         showTooltip(event);
+//     } else if(event.target.nextElementSibling.classList.contains('tooltip')){  //
+//         event.target.nextElementSibling.remove(); //style.display = "none";
+//     } else {
+//         showTooltip(event)
+//     }
+
+// })
+
+function showTooltip(event){
+    toolTip.style.display = 'block';
+    toolTip.innerText = event.target.title;
     toolTip.style.display = "block";  
 
-    let coords = getCoords(ev.target);
+    let coords = getCoords(event.target);
     toolTip.style.left = coords.left + "px";
-    toolTip.style.top = coords.bottom + "px";
+    toolTip.style.top = coords.top + 18 + "px";
     
-    ev.target.after(toolTip);
+    event.target.after(toolTip);
 }
 
-body.addEventListener('click', (event)=>{
-    event.preventDefault();
-    if(event.target.nextElementSibling == null){
-        showTooltip(event);
-    } else if(event.target.nextElementSibling.classList.contains('tooltip')){  //
-        event.target.nextElementSibling.remove(); //style.display = "none";
-    } else {
+hasToolTip.forEach(element =>{
+    element.addEventListener('click', (event)=>{
+        event.preventDefault();
         showTooltip(event)
-    }
-
+    })
 })
-
